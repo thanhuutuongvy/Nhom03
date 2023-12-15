21af86f4523b947acf9096e18b54798fa37e0bc3
thanhuutuongvy <thanhuutuongvy0710@gmail.com>
2023-12-15 17:38:58 +0700

    them reponsitive

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
+let menu = document.querySelector('#menu');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
 let searchForm = document.querySelector('.search-form');
 document.querySelector('#timkiem').onclick = () => {
   searchForm.classList.toggle('active');
